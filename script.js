31f3fc0e5322984f32dd2e38ada1bbee299d69c8
Home_Laptop <Jimmlius923@gmail.com>
2023-02-08 20:03:48 +0100

    Created function operate() and added it to 'equal' button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector(".input-display")
 const numberButtons = document.querySelectorAll('.numbers')
 const operators = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equal')
 
 let inputs = [];
 let currentOperator = ''
@@ -18,7 +19,7 @@ numberButtons.forEach(button => {
 operators.forEach (operator => {
     operator.addEventListener('click', () => {
         if (inputs.length == 0) {
-            inputs.push(`${display.textContent}`);
+            inputs.push(`+ ${display.textContent}`);
         } else {
             inputs.push(`${currentOperator} ${display.textContent}`);
         }
@@ -43,3 +44,25 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function operate() {
+    return inputs.reduce((ans, number) => {
+        a = ans;
+        b = parseInt(number.slice(2));
+
+        if (number.includes('/ ')) {
+            ans = divide(a, b);
+        } else if (number.includes('x ')) {
+            ans = multiply(a, b);
+        } else if (number.includes('+ ')) {
+            ans = add(a, b);
+        } else if (number.includes('- ')) {
+            ans = subtract(a, b);
+        }
+        return ans
+    }, 0)
+}
+
+equal.addEventListener('click', () => {
+    display.textContent = operate()
+})
