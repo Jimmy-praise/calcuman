7838564b1548cad2828be9230f95611e7377bd17
PJimmy_McP_Small <Jimmlius923@gmail.com>
2023-03-03 14:57:55 +0100

    Display number inputs and result while calculating is working fine

--- script.js
+++ script.js
@@ -11,17 +11,21 @@ numberButtons.forEach(button => {
         if (resultDisplay.textContent.length == 12) {
 
         }
-        else if (resultDisplay.textContent.includes('0.')) {
-            resultDisplay.textContent += button.getAttribute('key');
-        }
         else if (resultDisplay.textContent == result) {
             resultDisplay.textContent = button.getAttribute('key');
             inputs = [];
             inputsDisplay.textContent = '';
         }
-        else if (resultDisplay.textContent == '0' || resultDisplay.textContent == operate()) {
+        else if (resultDisplay.textContent == '0' || resultDisplay.textContent == operate() ) {
             resultDisplay.textContent = button.getAttribute('key');
-        } else {
+        } 
+        else if (resultDisplay.textContent == operate().slice(1)) {
+            resultDisplay.textContent = button.getAttribute('key');
+        }
+        else if (resultDisplay.textContent.includes('0.')) {
+            resultDisplay.textContent += button.getAttribute('key');
+        }
+        else {
             resultDisplay.textContent += button.getAttribute('key');
         };
     });
@@ -52,13 +56,14 @@ decimal.addEventListener('click', () => {
         resultDisplay.textContent = '0.';
         inputs = [];
         inputsDisplay.textContent = '';
-    }
+    } 
+    else if (resultDisplay.textContent == '0' || resultDisplay.textContent == operate()) {
+        resultDisplay.textContent = '0.';
+    } 
     else if (resultDisplay.textContent.length == 11 || resultDisplay.textContent.includes('.')) {
 
-    }
-    else if (resultDisplay.textContent == '0' || resultDisplay.textContent == operate()) {
-        resultDisplay.textContent += '.';
-    } else {
+    } 
+    else {
         resultDisplay.textContent += decimal.getAttribute('key')
     };
 })
@@ -69,14 +74,21 @@ operators.forEach (operator => {
     operator.addEventListener('click', () => {
         if (inputs.length == 0) {
             inputs.push(`+ ${resultDisplay.textContent}`);
-        } else {
+            inputsDisplay.textContent = `${resultDisplay.textContent} ${operator.textContent} `;
+            resultDisplay.textContent = operate();
+        } 
+        else if (resultDisplay.textContent == result) {
+            resultDisplay.textContent = operate().slice(1);
+            inputs = [`+ ${resultDisplay.textContent}`];
+            inputsDisplay.textContent = `${resultDisplay.textContent} ${operator.textContent} `
+        }
+        else {
             inputs.push(`${currentOperator} ${resultDisplay.textContent}`);
+            inputsDisplay.textContent += `${resultDisplay.textContent} ${operator.textContent} `;
+            resultDisplay.textContent = operate();
         };
 
-        inputsDisplay.textContent += `${resultDisplay.textContent} ${operator.textContent} `
-
         currentOperator = `${operator.textContent}`;
-        resultDisplay.textContent = operate();
 
     });
 });
@@ -114,9 +126,11 @@ function operate() {
         return ans;
     }, 0)
     if (result.toString().length > 12) {
-        return result.toExponential(9);
+        return result.toExponential(2);
     } else {
-        return result;
+        return ` ${result} `;
+        // spaces was added before result so that no number will be equal to operate() except operate() itself. It allows to calculate same numbers e.g. 2 x 22.
+        // It was also added so that no number will be equal to operate().slice(1) execpt operate().slice(1) itself.
     }
 }
 
